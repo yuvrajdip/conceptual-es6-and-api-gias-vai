7110b19d2de3b4fcdd9bd67fa012768e81a89d6c
yuvrajdip <dipbhowmik2018@gmail.com>
2023-09-02 12:27:29 +0600

    handleModal() modal

--- js/index.js
+++ js/index.js
@@ -33,6 +33,7 @@ const handleLoadNews = async (category_id) => {
   data.sort((a,b) => b.total_view - a.total_view ) // sorting array of objects according to a specific value
 
   const cardContainer = document.getElementById('card-container')
+  cardContainer.textContent = ''
 
   data.forEach(news => {
     console.log(news);
@@ -44,8 +45,10 @@ const handleLoadNews = async (category_id) => {
           <h2 class="card-title">${news.title.slice(0, 40) + "..."}</h2>
           <p>${news.details.slice(0, 60) + "..."}</p>
           <p>Total views : ${news.total_view===0 ? 'no views' : news.total_view }</p>
-          <div class="card-actions justify-end">
-            <button class="btn btn-primary">Show Details</button>
+          <div class=" justify-end">
+            
+            <button onclick="handleModal()" class="btn btn-secondary">See Details</button>        
+            
           </div>
         </div>
       </div>
@@ -57,4 +60,31 @@ const handleLoadNews = async (category_id) => {
 
 
 
+
+const handleModal = () => {
+  console.log(`modal`);
+
+  const modalContainer = document.getElementById('modal-container')
+
+  const div = document.createElement('div')
+  div.innerHTML = `
+    <!-- Open the modal using ID.showModal() method -->
+    <dialog id="my_modal_1" class="modal">
+      <form method="dialog" class="modal-box">
+        <h3 class="font-bold text-lg">Hello!</h3>
+        <p class="py-4">Press ESC key or click the button below to close</p>
+        <div class="modal-action">
+          <!-- if there is a button in form, it will close the modal -->
+          <button class="btn">Close</button>
+        </div>
+      </form>
+    </dialog>
+  `
+  modalContainer.appendChild(div)
+
+  const modal = document.getElementById('my_modal_1')
+  modal.showModal()
+}
+
+// handleLoadNews("01")
 handleCategory()
